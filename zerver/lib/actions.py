039f0a0ae635a333b8a1ae4a322772892ff685dc
Tanonic <noprocraft@gmail.com>
2017-12-17 13:22:18 +0100

    changes 'welcome' message
    1. part of gci task

--- zerver/lib/actions.py
+++ zerver/lib/actions.py
@@ -1639,6 +1639,9 @@ def check_send_message(sender, client, message_type_name, message_to,
         message_type_name,
         message_to,
         topic_name)
+    
+    if message_content == "welcome" || "Welcome":
+        message_content = "Welcome to Zulip :octopus:"
 
     message = check_message(sender, client, addressee,
                             message_content, realm, forged, forged_timestamp,
